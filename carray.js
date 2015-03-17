a23f99da61ba8aa8d7fc2bb4dc67ef7d4008b229
lgrube <lgv9483@saintjoe.edu>
2015-03-17 16:28:46 -0500

    added shellsort2

--- carray.js
+++ carray.js
@@ -122,3 +122,21 @@ function shellsort() {
 	   print(this.toString());
 	}
 }
+
+function shellsort2() {
+	var N = this.dataStore.length;
+	var h = 1;
+	while (h < N/3) {
+		 h = 3 * h + 1;
+	}
+	while (h >= 1) {
+	    for (var i = h; i < N; i++) {
+	        for (var j = i; j >= h && this.dataStore[j] < this.dataStore[j-h];
+			j -= h) {
+			swap(this.dataStore, j, j-h);
+		}
+	    }
+	    h = (h-1)/3;
+	}
+}
+
